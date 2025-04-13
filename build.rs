13970fa673fd6094c0e139081ac77150567e24cc
yannis300307 <yannis300307@gmail.com>
2025-04-13 11:34:52 +0200

    Fix CI

--- build.rs
+++ build.rs
@@ -3,10 +3,14 @@ use std::process::Command;
 fn main() {
     // Turn icon.png into icon.nwi
     println!("cargo:rerun-if-changed=src/icon.png");
-    let output = Command::new("cmd")
-        .args(&["/c", "nwlink", "png-nwi", "src/icon.png", "target/icon.nwi"])
-        .output()
-        .expect("Failure to launch process");
+    let output = 
+    {
+        if let Ok(out) = Command::new("nwlink").args(["png-nwi", "src/icon.png", "target/icon.nwi"]).output() {out}
+        else {
+            Command::new("cmd").args(["/c", "nwlink", "png-nwi", "src/icon.png", "target/icon.nwi"]).output().expect("Unable to convert icon.")
+        }
+        
+    };
     assert!(
         output.status.success(),
         "{}",
