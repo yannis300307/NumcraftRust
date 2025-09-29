c5a17229e323204b3d69b4d3f2c66fec10461483
yannis300307 <52321382+yannis300307@users.noreply.github.com>
2025-09-29 17:22:43 +0200

    Add new blocks

--- src/constants.rs
+++ src/constants.rs
@@ -99,6 +99,12 @@ pub enum BlockType {
     Stone = 1,
     Grass = 2,
     Dirt = 3,
+    Sand = 4,
+    Cobblestone = 5,
+    Border = 6,
+    Log = 7,
+    Leaves = 8,
+    Planks = 9,
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Serialize, Deserialize)]
@@ -109,15 +115,28 @@ pub enum ItemType {
     StoneBlock = 1,
     GrassBlock = 2,
     DirtBlock = 3,
+    SandBlock = 4,
+    CobblestoneBlock = 5,
+    BorderBlock = 6,
+    LogBlock = 7,
+    LeavesBlock = 8,
+    PlanksBlock = 9,
 }
 
 impl ItemType {
     pub fn get_texture_id(&self) -> u8 {
         match *self {
+            ItemType::Air => 0,
+
             ItemType::StoneBlock => 1,
             ItemType::GrassBlock => 2,
-            ItemType::DirtBlock => 3,
-            _ => 0,
+            ItemType::DirtBlock => 3, // 4 is the other texture of the grass block
+            ItemType::SandBlock => 5,
+            ItemType::CobblestoneBlock => 6,
+            ItemType::BorderBlock => 7,
+            ItemType::LogBlock => 8,
+            ItemType::LeavesBlock => 9,
+            ItemType::PlanksBlock => 10,
         }
     }
 
@@ -127,6 +146,12 @@ impl ItemType {
             ItemType::StoneBlock => 64,
             ItemType::GrassBlock => 64,
             ItemType::DirtBlock => 64,
+            ItemType::SandBlock => 64,
+            ItemType::CobblestoneBlock => 64,
+            ItemType::BorderBlock => 64,
+            ItemType::LogBlock => 64,
+            ItemType::LeavesBlock => 64,
+            ItemType::PlanksBlock => 64,
         }
     }
 
@@ -136,6 +161,12 @@ impl ItemType {
             ItemType::StoneBlock => Some(BlockType::Stone),
             ItemType::GrassBlock => Some(BlockType::Grass),
             ItemType::DirtBlock => Some(BlockType::Dirt),
+            ItemType::SandBlock => Some(BlockType::Sand),
+            ItemType::CobblestoneBlock => Some(BlockType::Cobblestone),
+            ItemType::BorderBlock => Some(BlockType::Border),
+            ItemType::LogBlock => Some(BlockType::Log),
+            ItemType::LeavesBlock => Some(BlockType::Leaves),
+            ItemType::PlanksBlock => Some(BlockType::Planks),
         }
     }
 }
@@ -157,6 +188,12 @@ impl BlockType {
                 }
             }
             BlockType::Dirt => 3,
+            BlockType::Sand => 5,
+            BlockType::Cobblestone => 6,
+            BlockType::Border => 7,
+            BlockType::Log => 8,
+            BlockType::Leaves => 9,
+            BlockType::Planks => 10,
         }
     }
 
@@ -166,6 +203,12 @@ impl BlockType {
             1 => Some(BlockType::Stone),
             2 => Some(BlockType::Grass),
             3 => Some(BlockType::Dirt),
+            4 => Some(BlockType::Sand),
+            5 => Some(BlockType::Cobblestone),
+            6 => Some(BlockType::Border),
+            7 => Some(BlockType::Log),
+            8 => Some(BlockType::Leaves),
+            9 => Some(BlockType::Planks),
             _ => None,
         }
     }
@@ -176,6 +219,12 @@ impl BlockType {
             BlockType::Stone => 2.,
             BlockType::Grass => 1.2,
             BlockType::Dirt => 1.,
+            BlockType::Sand => 1.,
+            BlockType::Cobblestone => 2.2,
+            BlockType::Border => -1.,
+            BlockType::Log => 1.5,
+            BlockType::Leaves => 0.3,
+            BlockType::Planks => 1.2,
         }
     }
 
@@ -185,16 +234,28 @@ impl BlockType {
             BlockType::Stone => ItemType::StoneBlock,
             BlockType::Grass => ItemType::DirtBlock,
             BlockType::Dirt => ItemType::DirtBlock,
+            BlockType::Sand => ItemType::SandBlock,
+            BlockType::Cobblestone => ItemType::CobblestoneBlock,
+            BlockType::Border => ItemType::BorderBlock,
+            BlockType::Log => ItemType::LogBlock,
+            BlockType::Leaves => ItemType::LeavesBlock,
+            BlockType::Planks => ItemType::PlanksBlock,
         }
     }
 }
 
 pub fn get_quad_color_from_texture_id(id: u8) -> Color {
     match id {
-        0 => Color::from_888(0, 0, 0),
         1 => Color::from_888(160, 160, 160),
         2 => Color::from_888(21, 147, 0),
         3 => Color::from_888(120, 77, 49),
+        5 => Color::from_888(208, 199, 6),
+        6 => Color::from_888(178, 178, 178),
+        7 => Color::from_888(19, 19, 19),
+        8 => Color::from_888(79, 53, 30),
+        9 => Color::from_888(36, 75, 37),
+        10 => Color::from_888(152, 124, 61),
+
         _ => Color::from_888(0, 0, 0),
         // 255 is reserved for block outline
     }
